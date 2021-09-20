361ea19e074e1c1c5b948f182cd2a5ca8219d211
Jason Fernandes <18moneyj@gmail.com>
2021-09-19 23:02:16 -0500

    finished

---
+++ A2JasonFernandes.appstudio/forms/switch/switch.js
@@ -0,0 +1,24 @@
+
+//Variable definition
+let name = prompt("Enter your first name:")
+let letters = prompt("Enter the state you live in (i.e. NE or FL).")
+let farhen = prompt("Enter the temperature in Fahrenheit today.")
+let sentence = ["wear a warm coat, hat, scarf, and gloves.", "wear a warm coat but you won't need a hat, scarf or globes.", "wear your warmest coat, a warm hat, a scarf, and warm gloves.", "wear a warm coat, hat and gloves. Maybe a scarf too."]
+
+//Logic to tell the user what to do
+ switch (true) {
+  case(farhen <= 32):
+    console.log(`${name}, ${sentence[0]}`)
+    break
+  case (farhen >= 32 && farhen <= 50 && letters == 'NE'):
+    console.log(`${name}, ${sentence[1]}`)
+    break
+ case (farhen >= 32 && farhen <= 50 && letters == 'FL'):
+    console.log(`${name}, ${sentence[2]}`)
+    break
+ case (farhen >= 50 && farhen <= 70):
+    console.log(`${name}, ${sentence[3]}`)
+    break
+  default:
+    console.log(`${name}, have a nice day`)
+}
